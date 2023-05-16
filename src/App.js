170359cd0186b7f6bcdc69a375715371dca1826d
Luis Ron <116908552+LuisRon27@users.noreply.github.com>
2023-05-16 16:15:42 -0300

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Blog from './pages/Blog';
+import Pricing from './pages/Pricing';
+import Contact from './pages/Contact';
+
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './Navigation.css';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+
+    const [navList, setnavList] = useState(false);
+
+    return (
+        <div className='container flex header' >
+
+            <div className='logo'>
+                <img src="./images/logo.png" alt="Logo" />
+            </div>
+
+            <nav className='nav'>
+                <ul className={navList? "small" : "flex"}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/services">Services</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/pricing">Pricing</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+
+                </ul>
+            </nav>
+
+            <div className='button flex'>
+                <h4>
+                    <span>2</span> My List
+                </h4>
+                <button className='btn1'>
+                    <i className='fa fa-sign-out'></i> Sign In
+                </button>
+            </div>
+
+            <div className='toggle'>
+                {/** cambia el estado de false a true*/}
+                <button onClick={() => setnavList(!navList)}>
+                    {navList ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
+                </button>
+            </div>
+            
+
+        </div>
+    );
+}
+
+export default Navigation;
